d38891a642c965877883e49d30559b82f858b907
Dilip <86300815+DilipPushparajan@users.noreply.github.com>
2025-12-23 08:36:23 +0000

    used prompts as advised for Step 4

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -17,10 +17,42 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+import os
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .views import UserViewSet, TeamViewSet, ActivityViewSet, LeaderboardViewSet, WorkoutViewSet, api_root
 
 
-router = DefaultRouter()
+
+# Patch the router to use the codespace URL in browsable API root links
+class CodespaceRouter(DefaultRouter):
+    def get_api_root_view(self, api_urls=None):
+        api_root_view = super().get_api_root_view(api_urls)
+        codespace_name = os.environ.get('CODESPACE_NAME')
+        base_url = None
+        if codespace_name:
+            base_url = f"https://{codespace_name}-8000.app.github.dev"
+
+        def view(request, *args, **kwargs):
+            # Patch request.build_absolute_uri to use codespace URL if available
+            if base_url:
+                orig_build_absolute_uri = request.build_absolute_uri
+                def custom_build_absolute_uri(location=None):
+                    uri = orig_build_absolute_uri(location)
+                    # Replace the scheme+host with codespace URL
+                    if uri.startswith('http://') or uri.startswith('https://'):
+                        # Find the first single slash after scheme
+                        idx = uri.find('/', uri.find('//') + 2)
+                        if idx != -1:
+                            uri = base_url + uri[idx:]
+                        else:
+                            uri = base_url
+                    return uri
+                request.build_absolute_uri = custom_build_absolute_uri
+            return api_root_view(request, *args, **kwargs)
+        return view
+
+router = CodespaceRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'teams', TeamViewSet, basename='team')
 router.register(r'activities', ActivityViewSet, basename='activity')
